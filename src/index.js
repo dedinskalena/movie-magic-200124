c83196330c5a2c6615dfd9c43dda71ee1ddd07ca
dedinskalena <35485425+dedinskalena@users.noreply.github.com>
2024-01-20 15:23:59 +0200

    config express-handelbars

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 const express=require('express')
 const app=express()
 const port=5000
+const handlebars=require('express-handlebars')
+const path=require('path')
+
+app.engine('hbs',handlebars.engine({
+    extname:'hbs'
+}))
+app.set('view engine','hbs')
+app.set('views',path.join(__dirname,'views'))
+
+
+
 app.get('/',(req,res)=>{
-    res.send('Home page')
+    res.render('home',{layout:false})
 })
 app.listen(port,()=>console.log('Server listening on port 5000'))
