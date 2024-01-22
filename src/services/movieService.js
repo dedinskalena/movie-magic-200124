6d25b7581dff62a1d32428de47b4a207abebafd0
dedinskalena <35485425+dedinskalena@users.noreply.github.com>
2024-01-22 15:02:47 +0200

    add details page

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -13,6 +13,10 @@ const movies=[{
 exports.getAll=()=>{
     return movies.slice()
 }  
+exports.getOne=(movieId)=>{
+    const movie=movies.find(m=>m._id==movieId)
+    return movie
+}
 exports.create=(movieData)=>{
     movieData._id=movies[movies.length-1]._id+1
     movies.push(movieData)
