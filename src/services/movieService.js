57021d4403245e2b5e4ac4c9790dd2c4f26108db
dedinskalena <35485425+dedinskalena@users.noreply.github.com>
2024-01-22 07:35:41 +0200

    add movie id and details link

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,4 +1,5 @@
 const movies=[{
+    _id:1,
     title: 'Jungle Cuise',
     genre: 'Adventure',
     director: 'Lukas',
@@ -13,6 +14,7 @@ exports.getAll=()=>{
     return movies.slice()
 }  
 exports.create=(movieData)=>{
+    movieData._id=movies[movies.length-1]._id+1
     movies.push(movieData)
 }
 
