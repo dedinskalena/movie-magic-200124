3a8a42536239212b7adc168bcd742a6f3597f53f
dedinskalena <35485425+dedinskalena@users.noreply.github.com>
2024-01-25 18:15:12 +0200

    install mongoose
    DB and model

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express=require('express')
+const mongoose=require('mongoose')
+const Movie=require('./models/Movie.js')
 const app=express()
 
 const configHandelbars=require('./config/configHandelbars.js')
@@ -14,5 +16,11 @@ configExpress(app)
 
 app.use(routes)
 
+mongoose.connect(`mongodb://127.0.0.1:27017/magic-movies`)
+.then(()=>{
+    console.log('DB connected')
+    app.listen(port,()=>console.log('Server listening on port 5000'))
+})
+.catch(err=>console.log('Can not connect to DB'))
+//mongoose.connection.on('error',(err)=>console.error(err))
 
-app.listen(port,()=>console.log('Server listening on port 5000'))
