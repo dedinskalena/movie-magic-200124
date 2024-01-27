39cbdf10ecb3b4d40cc7de7d1189eded7e387306
dedinskalena <35485425+dedinskalena@users.noreply.github.com>
2024-01-27 11:47:45 +0200

    add post for cast create

--- src/controllers/castController.js
+++ src/controllers/castController.js
@@ -1,7 +1,13 @@
 const router=require('express').Router()
 
-router.get('/cast/sreate',(req,res)=>{
-    
+router.get('/create',(req,res)=>{
+    res.render('cast/create')
+})
+
+router.post('/create',(req,res)=>{
+    const body=req.body
+    console.log(body)
+    res.redirect('/')
 })
 
 module.exports=router
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 const router=require('express').Router()
 const homeContoller=require('./controllers/homeController')
 const movieController=require('./controllers/movieController')
+const castController=require('./controllers/castController')
 
 router.use(homeContoller)
 router.use (movieController)
+router.use('/cast',castController)
 
 router.get('*',(req,res)=>{
     res.render('404')
