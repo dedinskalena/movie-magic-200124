49f49c12bca1fd6c8fb6e8f796ce07df9b214026
dedinskalena <35485425+dedinskalena@users.noreply.github.com>
2024-02-06 20:08:07 +0200

    generate token

--- src/models/User.js
+++ src/models/User.js
@@ -6,7 +6,8 @@ const userSchema=new mongoose.Schema({
     email:{
         type:String,
         required:true,
-        tolowercase:true
+        tolowercase:true,
+        unique:true
 
     },
     password:{
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,3 +1,30 @@
 const User=require('../models/User')
+const bcrypt=require('bcrypt')
+const jwt=require('../lib/jwt')
+const secret='afdhfkflboirnmvmxvs'
 
 exports.register=(userData)=>User.create(userData)
+exports.login=async (email,password)=>{
+    //get user from db
+    const user=await User.findOne({email})
+
+    //check if user exist
+    if(!user){
+        throw new Error('Can not find email or password')
+    }
+
+    //check if password is valid
+    const isValid=await bcrypt.compare(password,user.password)
+    if(!isValid){
+        throw new Error('Can not find email or password')
+    }
+
+    //generate JWT token and retern token
+    const payload={
+        _id:user._id,
+        email:user.email
+    }
+     const token=await jwt.sign(payload,secret,{expiresIn:'2h'})
+     return token
+
+}
